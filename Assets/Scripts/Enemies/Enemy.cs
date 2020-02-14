b295ddfacff8113dc65cfa17e016d198f22d6f73
Taylor Anderson <taylor-anderson@hotmail.ca>
2020-02-13 23:38:41 -0500

    Fixing enemy vs enemy collision deathsssss

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -13,21 +13,13 @@ public class Enemy : Pickup {
 
     for (int i = 0; i < results.Count; i++) {
       var col = results[i];
-      var otherSlime = col?.transform?.parent?.GetComponent<Slime>();
-      if (otherSlime && otherSlime.color == this.color && otherSlime != this && !hitWallAfterThrown) {
-        if (otherSlime.size == this.size && size < SlimeManager.instance.slimeProgression.Length - 1) {
-          var nextSlime = SlimeManager.instance.slimeProgression[size + 1];
-          var newSlimeObj = Instantiate(nextSlime);
-          //align bottoms
-          var midPos = (transform.position + otherSlime.transform.position) / 2;
-          var newSlime = newSlimeObj.GetComponent<Slime>();
-          newSlime.SetColor(this.color);
-          newSlime.transform.position = Vector3.right * transform.position.x + Vector3.up * (GetBottom().y + newSlime.spriteRenderer.bounds.extents.y + 0.01f);
-          this.toBeDeleted = otherSlime.toBeDeleted = true;
-          newSlime.fsm.ChangeState(SlimeState.Combined);
-          Destroy(gameObject);
-          Destroy(otherSlime.gameObject);
-        }
+      var otherEnemy = col?.transform?.parent?.GetComponent<Enemy>();
+      if (!otherEnemy) otherEnemy = col.gameObject.GetComponent<Enemy>();
+      if (otherEnemy && otherEnemy != this && !hitWallAfterThrown) {
+        this.currentGravityModifier = 15;
+        otherEnemy.currentGravityModifier = 15;
+        otherEnemy.Die(throwDir);
+        this.Die(-throwDir);
       }
     }
 
